ae3a266c9aceaed1a377e5039b73c04cbfd693fd
Jake911 <infernioboss@yandex.ru>
2018-12-10 01:56:49 +0300

    smallfixes

---
+++ Projects/js/custom.js
@@ -0,0 +1,74 @@
+
+// Bind to scroll
+$(window).scroll(function() {
+  navFunction();
+});
+
+
+var navbar = document.getElementById("navbarSticky");
+
+var navNumber = document.getElementById("navPhone");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function navFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+    navNumber.classList.add("phoneHidden");
+  } else {
+    navbar.classList.remove("sticky");
+    navNumber.classList.remove("phoneHidden");
+  }
+}
+
+function createPost(element) {
+  return document.createElement(element);
+}
+
+function append(parent, element) {
+  return parent.appendChild(element);
+}
+
+// fetch(`http://185.220.34.243/getposts`, {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json"
+//   },
+//   body: JSON.stringify({
+//     date: Date.now()
+//   })
+// })
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(posts) {
+//     return posts.map(function(post) {
+//       var div = createPost("div");
+//       var article = createPost("article");
+//       var h2 = createPost("h2");
+//       article.innerHTML = post.body;
+//       h2.innerHTML = post.title;
+//       append(div, h2);
+//       append(div, article);
+//       append(document.getElementById("news"), div);
+//       div.classList.add("col-md-12");
+//     });
+//   });
+
+fetch("http://185.220.34.243/getcontacts")
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (settings) {
+    return settings.map(function (contacts) {
+      var phone = document.getElementsByClassName("navPhone");
+      var mail = document.getElementsByClassName("navMail");
+      for (var i = 0; i < phone.length; i++) {
+        phone[i].innerHTML = contacts.phone;
+        mail[i].innerHTML = contacts.MAIL.USER;
+        mail[i].href = "mailto:" + contacts.MAIL.USER;
+      }
+    });
+  });
