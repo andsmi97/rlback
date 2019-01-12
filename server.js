47d35e4009d55e37231d61a833492e5b38dfa6ef
Andrew Smirnov <andsmi97@yandex.ru>
2019-01-12 17:58:59 +0300

    changed build

--- server.js
+++ server.js
@@ -1,82 +1,82 @@
 //libs
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const morgan = require("morgan");
-const helmet = require("helmet");
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
 
 //Controllers
-const email = require("./controllers/Email");
-const tenant = require("./controllers/Tenant");
-const posts = require("./controllers/Posts");
-const projects = require("./controllers/Projects");
-const settings = require("./controllers/Settings");
-const tariffs = require("./controllers/Tariffs");
-const auth = require("./controllers/Auth");
-const sections = require("./controllers/Sections");
+const email = require('./controllers/Email');
+const tenant = require('./controllers/Tenant');
+const posts = require('./controllers/Posts');
+const projects = require('./controllers/Projects');
+const settings = require('./controllers/Settings');
+const tariffs = require('./controllers/Tariffs');
+const auth = require('./controllers/Auth');
+const sections = require('./controllers/Sections');
 const app = express();
 
 //Middleware
 app.use(bodyParser.json());
 app.use(cors());
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 app.use(helmet());
-app.use("/admin", express.static("build"));
-app.use("/", express.static("ozerodom.ru"));
-app.use("/news", express.static("News"));
-app.use("/projects", express.static("Projects"));
+app.use('/admin', express.static('build'));
+app.use('/', express.static('ozerodom.ru'));
+app.use('/news', express.static('News'));
+app.use('/projects', express.static('Projects'));
 //Routes
 
 //Tenants
-app.post("/tenantinsert", tenant.handleInsert);
-app.post("/tenantupdate", tenant.handleUpdate);
-app.delete("/tenantdelete", tenant.handleDelete);
-app.get("/tenants", tenant.handleSelect);
+app.post('/tenantinsert', tenant.handleInsert);
+app.post('/tenantupdate', tenant.handleUpdate);
+app.delete('/tenantdelete', tenant.handleDelete);
+app.get('/tenants', tenant.handleSelect);
 
 //Mail
-app.post("/mail", email.handleSend);
+app.post('/mail', email.handleSend);
 
 //Posts
-app.post("/addpost", posts.addPost);
-app.delete("/deletepost", posts.deletePost);
-app.patch("/updatepost", posts.updatePost);
-app.post("/getposts", posts.getPosts); //since i can't use body in GET
-app.patch("/updatepostphoto", posts.updatePostPhoto);
-app.delete("/deletePostPhoto", posts.deletePostPhoto);
+app.post('/addpost', posts.addPost);
+app.delete('/deletepost', posts.deletePost);
+app.patch('/updatepost', posts.updatePost);
+app.post('/getposts', posts.getPosts); //since i can't use body in GET
+app.patch('/updatepostphoto', posts.updatePostPhoto);
+app.delete('/deletePostPhoto', posts.deletePostPhoto);
 
 //Projects
-app.post("/addproject", projects.addProject);
-app.delete("/deleteproject", projects.deleteProject);
-app.patch("/updateproject", projects.updateProject);
-app.post("/getprojects", projects.getProjects); //since i can't use body in GET
-app.patch("/updateprojectphoto", projects.updateProjectPhoto);
-app.delete("/deleteProjectPhoto", projects.deleteProjectPhoto);
+app.post('/addproject', projects.addProject);
+app.delete('/deleteproject', projects.deleteProject);
+app.patch('/updateproject', projects.updateProject);
+app.post('/getprojects', projects.getProjects); //since i can't use body in GET
+app.patch('/updateprojectphoto', projects.updateProjectPhoto);
+app.delete('/deleteProjectPhoto', projects.deleteProjectPhoto);
 
 //Settings
-app.put("/changeaccountpassword", settings.changeAccountPassword);
-app.put("/updateemailcredentials", settings.updateEmailCredentials);
+app.put('/changeaccountpassword', settings.changeAccountPassword);
+app.put('/updateemailcredentials', settings.updateEmailCredentials);
 //Tariffs
-app.put("/changetariffs", tariffs.changeTariffs);
+app.put('/changetariffs', tariffs.changeTariffs);
 //Auth
-app.post("/createuser", auth.addUser);
-app.post("/login", auth.login);
+app.post('/createuser', auth.addUser);
+app.post('/login', auth.login);
 //TODO: DELETE IN PRODUCTION
-app.get("/users", auth.getUsers);
-app.post("/deleteuser", auth.deleteUser);
-app.get("/getcontacts", auth.getContacts);
+app.get('/users', auth.getUsers);
+app.post('/deleteuser', auth.deleteUser);
+app.get('/getcontacts', auth.getContacts);
 
-app.post("/addimg", sections.addPhoto);
-app.post("/addsiteSections", sections.addSiteSections);
-app.post("/sectionPhotos", sections.sectionPhotos);
-app.delete("/deletePhoto", sections.deletePhoto);
-app.post("/updatePhoto", sections.updatePhoto);
-app.post("/siteContent", sections.siteContent);
-app.post("/uploaddefault", sections.addDefaultPhotos);
-app.post("/clearall", sections.clearAll);
-app.patch("/reorderPhotos", sections.reorderPhotos);
+app.post('/addimg', sections.addPhoto);
+app.post('/addsiteSections', sections.addSiteSections);
+app.post('/sectionPhotos', sections.sectionPhotos);
+app.delete('/deletePhoto', sections.deletePhoto);
+app.post('/updatePhoto', sections.updatePhoto);
+app.post('/siteContent', sections.siteContent);
+app.post('/uploaddefault', sections.addDefaultPhotos);
+app.post('/clearall', sections.clearAll);
+app.patch('/reorderPhotos', sections.reorderPhotos);
 
 //Don't stop server in production
-process.on("uncaughtException", err => {
+process.on('uncaughtException', err => {
   console.log(err);
 });
 
